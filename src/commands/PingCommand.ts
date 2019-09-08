13b1e8107187fb9a683191672db30165248deebc
InkoHX <inkohx@gmail.com>
2019-09-08 18:42:08 +0900

    作り直す

--- src/commands/PingCommand.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from 'discord.js'
-import { Command } from '../lib'
-
-export default class PingCommand extends Command {
-  public execute (message: Message): void {
-    message.reply('Pong')
-  }
-}
--- src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-import Command from './structures/Command'
-import Event from './structures/Event'
-import HydraQueen from './HydraQueen'
-
-export {
-  Command,
-  HydraQueen,
-  Event
-}
