737ecb4203055b8d5e192d168a6d2870dd566d7f
InkoHX <inkohx@gmail.com>
2019-09-12 10:09:38 +0900

    Update Event.ts

--- src/lib/structures/Event.ts
+++ src/lib/structures/Event.ts
@@ -8,11 +8,13 @@ export interface EventOptions {
 }
 
 export default abstract class Event {
+  public readonly client: HydraClient
   public readonly name: string
   public readonly once: boolean
   public readonly emitter: EventEmitter
 
   constructor (client: HydraClient, options: EventOptions) {
+    this.client = client
     this.name = options.name
     this.emitter = options.emitter || client
     this.once = options.once || false
